38d35c204da758d7d31ed50870483e06f889dc8e
pipinho13 <sergiopit5@hotmail.com>
2015-10-09 10:49:26 +0300

    Update rplot3.r

--- rplot3.r
+++ rplot3.r
@@ -1,11 +1,14 @@
 
 
 ####plot 3 
+###assuming that we have downloaded and unzip localy the dataset we create the table
 df <- tbl_df(read.table("household_power_consumption.txt", header=TRUE, sep= ";", na.strings = c("?","")))
+####create datetime
 df$Date <- as.Date(df$Date, format = "%d/%m/%Y")
 df$timetemp <- paste(df$Date, df$Time)
 df$Time <- strptime(df$timetemp, format = "%Y-%m-%d %H:%M:%S")
-dfsubset<-subset(df, Date >= "2007-02-01" & Date<= "2007-02-02")  
+####choose only the relevant dates
+dfsubset<-subset(df, Date >= "2007-02-01" & Date<= "2007-02-02") 
 
 png(file = "plot3.png", bg = "transparent", width = 480, height = 480)
 plot(dfsubset$Time, dfsubset$Sub_metering_1, type="l", ylab="Energy sub metering", xlab="")
