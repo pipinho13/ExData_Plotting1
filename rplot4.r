3f1e8ddeef26a1edf8fd2191e7a0c75483219f4c
pipinho13 <sergiopit5@hotmail.com>
2015-10-09 10:49:08 +0300

    Update rplot4.r

--- rplot4.r
+++ rplot4.r
@@ -1,9 +1,12 @@
 
-#####plot 4
+#####plot 4 
+###assuming that we have downloaded and unzip localy the dataset we create the table
 df <- tbl_df(read.table("household_power_consumption.txt", header=TRUE, sep= ";", na.strings = c("?","")))
+####create datetime
 df$Date <- as.Date(df$Date, format = "%d/%m/%Y")
 df$timetemp <- paste(df$Date, df$Time)
 df$Time <- strptime(df$timetemp, format = "%Y-%m-%d %H:%M:%S")
+####choose only the relevant dates
 dfsubset<-subset(df, Date >= "2007-02-01" & Date<= "2007-02-02") 
 
 png(file = "plot4.png", bg = "transparent", width = 480, height = 480)
